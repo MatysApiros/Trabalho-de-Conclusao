e23686aecf0bc03a330b3ab9493cb84d39d63684
Matheus Canabarro Peña <matheus.canabarro@hotmail.com>
2021-11-15 12:13:02 -0300

    Ajuste CORS

--- src/server.js
+++ src/server.js
@@ -16,6 +16,12 @@ mongoose.connect("mongodb+srv://apiros:whiix416EGE7ghQa@cluster0.witsr.mongodb.n
 
 const porta = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.set('port', porta);
 
 app.get('/emergencias', (req,res, next) => {
